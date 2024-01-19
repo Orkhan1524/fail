b972f29abbe78ffeab48d2c45d16e36e43a37ebe
Орхан <151119680+Orkhan1524@users.noreply.github.com>
2024-01-19 22:53:42 +0300

    Add files via upload

---
+++ задание 1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void saveToFile(const string& filename, const vector<string>& data) {
+    ofstream file(filename);
+    if (!file.is_open()) {
+        cout << "error";
+    }
+    else {
+        for (const string& word : data) {
+            file << word << endl;
+        }
+    }
+    file.close();
+}
+void loadFromFile(const string& filename, vector<string>& outData) {
+    ifstream file(filename);
+    if (file.is_open()) {
+        std::string word;
+        while (file >> word) {
+            outData.push_back(word);
+        }
+        file.close();
+    }
+    else {
+        cout << "error" << endl;
+    }
+}
+int main()
+{
+    string filename;
+    filename = "love.txt";
+    vector<string>data;
+    vector<string>data1;
+    data1.push_back("I");
+    data1.push_back("love");
+    data1.push_back("my");
+    data1.push_back("family");
+    saveToFile(filename, data);
+    loadFromFile(filename, data1);
+    cout << data[1] << endl;
+}
